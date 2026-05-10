b992ef76ae7153eeacf78420e93c54245fb49a9e
Phoenixmay23 <wushu2444@gmail.com>
2026-05-10 00:29:06 -0700

    edited game.js to include framework for 3 levels and a summary screen

--- game.js
+++ game.js
@@ -7,12 +7,43 @@ class Level1 extends Phaser.Scene{
     }
 }
 
-let config = {
-    type: Phaser.WEBGL, 
+class Level2 extends Phaser.Scene{
+    constructor(){
+        super('level2');
+    }
+    create() {
+
+    }
+}
+
+class Level3 extends Phaser.Scene{
+    constructor(){
+        super('level3');
+    }
+    create() {
+
+    }
+}
+
+class Summary extends Phaser.Scene{
+    constructor(){
+        super('summary');
+    }
+    create() {
+
+    }
+}
+
+const config = {
+    type: Phaser.AUTO, 
     width: 1920,
     height: 1080,
     backgroundColor: 0xd6bae0,
-    scene: [Level1],
+    physics: {
+        default: 'arcade',
+        arcade: {debug: false}
+    },
+    scene: [Level1, Level2, Level3, Summary],
 }
 
-let game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
